efc0d7d0adc98bae642e9947b7bf51998db41892
Greek <ygreek@gmail.com>
2020-09-08 03:53:58 +0800

    Added task and exception files

---
+++ src/main/java/duke/tasks/Deadline.java
@@ -0,0 +1,16 @@
+package duke.tasks;
+
+public class Deadline extends Task{
+
+	protected String deadlineDate;
+
+	public Deadline(String description, String deadlineDate) {
+	    super(description);
+	    this.deadlineDate = deadlineDate;
+	}
+
+	@Override
+	public String toString() {
+	    return "[D]" + super.toString() + "(by:" + deadlineDate + ")";
+	}
+}
