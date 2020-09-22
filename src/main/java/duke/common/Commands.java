447eff316c4aa40e6435f8ffd4f97414d4f078b4
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:37:08 +0800

    Update Commands.java

--- src/main/java/duke/common/Commands.java
+++ src/main/java/duke/common/Commands.java
@@ -4,12 +4,12 @@ package duke.common;
  * Container for commands
  */
 public class Commands {
-	public static final String COMMAND_LIST = "list";
-	public static final String COMMAND_TODO = "todo";
-	public static final String COMMAND_DEADLINE = "deadline";
-	public static final String COMMAND_EVENT = "event";
-	public static final String COMMAND_DONE = "done";
-	public static final String COMMAND_BYE = "bye";
-	public static final String COMMAND_DELETE = "delete";
-	public static final String COMMAND_FIND = "find";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_TODO = "todo";
+    public static final String COMMAND_DEADLINE = "deadline";
+    public static final String COMMAND_EVENT = "event";
+    public static final String COMMAND_DONE = "done";
+    public static final String COMMAND_BYE = "bye";
+    public static final String COMMAND_DELETE = "delete";
+    public static final String COMMAND_FIND = "find";
 }
