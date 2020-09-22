f393cff5c077dba5d125ba780eecd13e50ce234b
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:36:48 +0800

    Update TodoCommand.java

--- src/main/java/duke/commands/TodoCommand.java
+++ src/main/java/duke/commands/TodoCommand.java
@@ -11,21 +11,21 @@ import duke.tasks.ToDo;
  * Adds a todo to task list.
  */
 public class TodoCommand {
-	public static void addTodo(String task, boolean isDone, boolean isNew) throws DukeException {
-		try {
-			if(task == null) {
-				throw new DukeException(Errors.ERROR_EMPTY_TODO);
-			}
-			Task todo = new ToDo(task);
-			TaskList.addTask(todo);
-			if(isDone) {
-				todo.markAsDone();
-			}
-			if(isNew) {
-				TextUi.echoTask(todo);
-			}
-		} catch (StringIndexOutOfBoundsException e) {
-			TextUi.printError(Errors.ERROR_INVALID_INPUT);
-		}
-	}
+    public static void addTodo(String task, boolean isDone, boolean isNew) throws DukeException {
+        try {
+            if(task == null) {
+                throw new DukeException(Errors.ERROR_EMPTY_TODO);
+            }
+            Task todo = new ToDo(task);
+            TaskList.addTask(todo);
+            if(isDone) {
+                todo.markAsDone();
+            }
+            if(isNew) {
+                TextUi.echoTask(todo);
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            TextUi.printError(Errors.ERROR_INVALID_INPUT);
+        }
+    }
 }
