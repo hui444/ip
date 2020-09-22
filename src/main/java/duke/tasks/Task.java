a6f29fa6548aeee5d987c4516bffca04ed970359
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:50:21 +0800

    Update Task.java

--- src/main/java/duke/tasks/Task.java
+++ src/main/java/duke/tasks/Task.java
@@ -3,34 +3,34 @@ package duke.tasks;
 import java.util.Collection;
 
 public class Task {
-	public String description;
-	protected Boolean isDone;
+    public String description;
+    protected Boolean isDone;
 
-	public Task(String description) {
-	    this.description = description;
-	    this.isDone = false;
-	}
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 
-	public String getStatusIcon() {
-		//returns [tick] or [X] symbols
-	    return (isDone ? "[" + "\u2713" + "]" : "[" + "\u2718" + "]");
-	}
+    public String getStatusIcon() {
+        //returns [tick] or [X] symbols
+        return (isDone ? "[" + "\u2713" + "]" : "[" + "\u2718" + "]");
+    }
 
-	public void markAsDone() {
-	    this.isDone = true;
-	}
+    public void markAsDone() {
+        this.isDone = true;
+    }
 
-	@Override
-	public String toString() {
-	    return getStatusIcon() + " " + this.description;
-	}
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
 
-	public String fileString() {
-		int val = isDone ? 1 : 0;
-		return " | " + val + " | " + this.description;
-	}
+    public String fileString() {
+        int val = isDone ? 1 : 0;
+        return " | " + val + " | " + this.description;
+    }
 
-	public String getDescription() {
-		return this.description;
-	}
+    public String getDescription() {
+        return this.description;
+    }
 }
