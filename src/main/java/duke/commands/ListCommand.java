7af5b344ab9e30839887dfb4826a959e3233a676
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:35:00 +0800

    Update ListCommand.java

--- src/main/java/duke/commands/ListCommand.java
+++ src/main/java/duke/commands/ListCommand.java
@@ -8,16 +8,16 @@ import duke.common.Errors;
  * Prints task list.
  */
 public class ListCommand {
-	public static void printList() {
-		if (TaskList.arrTasks.size() == 0) {
-			TextUi.printError(Errors.ERROR_EMPTY_LIST);
-			return;
-		}
-		TextUi.printLine();
-		System.out.println("Here are the tasks in your list:");
-		for (int i = 0; i < TaskList.arrTasks.size(); i++) {
-			System.out.println((i+1) + ". " + TaskList.arrTasks.get(i));
-		}
-		TextUi.printLine();
-	}
+    public static void printList() {
+        if (TaskList.arrTasks.size() == 0) {
+            TextUi.printError(Errors.ERROR_EMPTY_LIST);
+            return;
+        }
+        TextUi.printLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < TaskList.arrTasks.size(); i++) {
+            System.out.println((i+1) + ". " + TaskList.arrTasks.get(i));
+        }
+        TextUi.printLine();
+    }
 }
