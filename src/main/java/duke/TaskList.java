fe28a4937bdac5097078774d34f9bf2d7273ef3d
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:23:52 +0800

    Update TaskList.java

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
  * Initialises and adds tasks to task list
  */
 public class TaskList {
-	public static ArrayList<Task> arrTasks = new ArrayList<>();
+    public static ArrayList<Task> arrTasks = new ArrayList<>();
 
-	public static void addTask(Task task) {
-		arrTasks.add(task);
-	}
+    public static void addTask(Task task) {
+        arrTasks.add(task);
+    }
 }
