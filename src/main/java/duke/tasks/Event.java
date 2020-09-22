f69f537b87b033e28f67b41bb95fd2b1670c3e87
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:49:18 +0800

    Update Event.java

--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,21 +1,21 @@
 package duke.tasks;
 
 public class Event extends Task {
-	protected String eventDate;
+    protected String eventDate;
 
-	public Event(String description, String eventDate) {
-	    super(description);
-	    this.eventDate = eventDate;
-	}
+    public Event(String description, String eventDate) {
+        super(description);
+        this.eventDate = eventDate;
+    }
 
-	@Override
-	public String toString() {
-	    return "[E]" + super.toString() + " (at: " + eventDate + ")";
-	}
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + eventDate + ")";
+    }
 
-	@Override
-	public String fileString() {
-		int val = isDone ? 1 : 0;
-		return "E | " + val + " | " + this.description + " | " + this.eventDate;
-	}
+    @Override
+    public String fileString() {
+        int val = isDone ? 1 : 0;
+        return "E | " + val + " | " + this.description + " | " + this.eventDate;
+    }
 }
