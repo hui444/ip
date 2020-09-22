b09a218a988206f788e747f28615fedb74f6fffe
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:47:44 +0800

    Update StorageFile.java

--- src/main/java/duke/storage/StorageFile.java
+++ src/main/java/duke/storage/StorageFile.java
@@ -11,20 +11,20 @@ import java.io.IOException;
  * Initialises file for storage
  */
 public class StorageFile {
-	public StorageFile(String filePath, String directoryName) {
-		File file = new File(filePath);
-		File directory = new File(directoryName);
+    public StorageFile(String filePath, String directoryName) {
+        File file = new File(filePath);
+        File directory = new File(directoryName);
 
-		try {
-			directory.mkdir();
-			file.createNewFile();
-			Storage.readFile(filePath);
-		} catch (FileNotFoundException e) {
-			TextUi.printError("File not found");
-		} catch (IOException e) {
-			TextUi.printError(e.getMessage());
-		} catch (DukeException e) {
-			TextUi.printError(e.errorMessage);
-		}
-	}
+        try {
+            directory.mkdir();
+            file.createNewFile();
+            Storage.readFile(filePath);
+        } catch (FileNotFoundException e) {
+            TextUi.printError("File not found");
+        } catch (IOException e) {
+            TextUi.printError(e.getMessage());
+        } catch (DukeException e) {
+            TextUi.printError(e.errorMessage);
+        }
+    }
 }
