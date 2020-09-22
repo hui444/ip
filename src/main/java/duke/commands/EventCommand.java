a39462db4316d8df490aa2ce51ff72326ae3d3af
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:32:18 +0800

    Update EventCommand.java

--- src/main/java/duke/commands/EventCommand.java
+++ src/main/java/duke/commands/EventCommand.java
@@ -11,35 +11,35 @@ import duke.tasks.Task;
  * Adds an event to task list.
  */
 public class EventCommand {
-	public static void addEvent(String task, boolean isDone, boolean isNew) throws DukeException {
-		try {
-			if (task == null) {
-				throw new DukeException(Errors.ERROR_EMPTY_EVENT);
-			}
-			if(!task.contains("/at")) {
-				throw new DukeException(Errors.ERROR_NO_DATE_EVENT);
-			}
-			String description = task.substring(0, task.indexOf("/at")).trim();
+    public static void addEvent(String task, boolean isDone, boolean isNew) throws DukeException {
+        try {
+            if (task == null) {
+                throw new DukeException(Errors.ERROR_EMPTY_EVENT);
+            }
+            if(!task.contains("/at")) {
+                throw new DukeException(Errors.ERROR_NO_DATE_EVENT);
+            }
+            String description = task.substring(0, task.indexOf("/at")).trim();
 
-			if(description.isEmpty()) {
-				throw new DukeException(Errors.ERROR_EMPTY_EVENT);
-			}
+            if(description.isEmpty()) {
+                throw new DukeException(Errors.ERROR_EMPTY_EVENT);
+            }
 
-			String eventDate = task.substring(task.indexOf("/at") + 3).trim();
+            String eventDate = task.substring(task.indexOf("/at") + 3).trim();
 
-			if(eventDate.isEmpty()) {
-				throw new DukeException(Errors.ERROR_NO_DATE_EVENT);
-			}
-			Task event = new Event(description, eventDate);
-			TaskList.addTask(event);
-			if(isDone) {
-				event.markAsDone();
-			}
-			if(isNew) {
-				TextUi.echoTask(event);
-			}
-		} catch (StringIndexOutOfBoundsException e) {
-			TextUi.printError(Errors.ERROR_INVALID_INPUT);
-		}
-	}
+            if(eventDate.isEmpty()) {
+                throw new DukeException(Errors.ERROR_NO_DATE_EVENT);
+            }
+            Task event = new Event(description, eventDate);
+            TaskList.addTask(event);
+            if(isDone) {
+                event.markAsDone();
+            }
+            if(isNew) {
+                TextUi.echoTask(event);
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            TextUi.printError(Errors.ERROR_INVALID_INPUT);
+    	}
+    }
 }
