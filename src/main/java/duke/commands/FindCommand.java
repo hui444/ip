f01a138fe3c49c86b5a26f4f80d1eef157736e47
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:34:09 +0800

    Update FindCommand.java

--- src/main/java/duke/commands/FindCommand.java
+++ src/main/java/duke/commands/FindCommand.java
@@ -14,29 +14,29 @@ import static java.util.stream.Collectors.toList;
  * Makes a new list containing keywords given by user
  */
 public class FindCommand {
-	public static ArrayList<Task> filterByKeyword(String keyword) {
-		ArrayList<Task> filteredList = (ArrayList<Task>) TaskList.arrTasks.stream()
-				.filter((s) -> s.getDescription().contains(keyword))
-				.collect(toList());
+    public static ArrayList<Task> filterByKeyword(String keyword) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) TaskList.arrTasks.stream()
+                .filter((s) -> s.getDescription().contains(keyword))
+                .collect(toList());
+        
+        return filteredList;
+    }
 
-		return filteredList;
-	}
+    public static void findTask(String keyword) throws DukeException {
+        if(keyword == null) {
+            throw new DukeException(Errors.ERROR_NO_KEYWORD);
+        }
+        ArrayList<Task> filteredTaskList = filterByKeyword(keyword);
+        
+        if(filteredTaskList.size() == 0) {
+            throw new DukeException(Errors.ERROR_NOT_FOUND + keyword);
+        }
 
-	public static void findTask(String keyword) throws DukeException {
-		if(keyword == null) {
-			throw new DukeException(Errors.ERROR_NO_KEYWORD);
-		}
-		ArrayList<Task> filteredTaskList = filterByKeyword(keyword);
-
-		if(filteredTaskList.size() == 0) {
-			throw new DukeException(Errors.ERROR_NOT_FOUND + keyword);
-		}
-
-		TextUi.printLine();
-		System.out.println("Here are the matching \"" + keyword + "\" tasks in your list:");
-		for (int i = 0; i < filteredTaskList.size(); i++) {
-			System.out.println((i+1) + ". " + filteredTaskList.get(i));
-		}
-		TextUi.printLine();
-	}
+        TextUi.printLine();
+        System.out.println("Here are the matching \"" + keyword + "\" tasks in your list:");
+        for (int i = 0; i < filteredTaskList.size(); i++) {
+            System.out.println((i+1) + ". " + filteredTaskList.get(i));
+        }
+        TextUi.printLine();
+    }
 }
