13bd273e023772466a0d02a39e2d05d0e9c44568
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:29:58 +0800

    Update DoneCommand.java

--- src/main/java/duke/commands/DoneCommand.java
+++ src/main/java/duke/commands/DoneCommand.java
@@ -10,17 +10,17 @@ import duke.exceptions.DukeException;
  * Marks specified task in task list as done.
  */
 public class DoneCommand {
-	public static void markAsDone(String inputNum) throws DukeException {
-		try {
-			int itemIndex = Parser.getIndexFromInput(inputNum,
-					"Please input a number to mark task as done.");
-			if(TaskList.arrTasks.get(itemIndex).getStatusIcon().equals("[\u2713]")) {
-				throw new DukeException(Errors.ERROR_MARKED_TASK);
-			}
-			TaskList.arrTasks.get(itemIndex).markAsDone();
-			TextUi.printDone(itemIndex);
-		} catch (NumberFormatException e) {
-			TextUi.printError(Errors.ERROR_INVALID_TASK_NUMBER);
-		}
-	}
+    public static void markAsDone(String inputNum) throws DukeException {
+        try {
+            int itemIndex = Parser.getIndexFromInput(inputNum,
+                    "Please input a number to mark task as done.");
+            if(TaskList.arrTasks.get(itemIndex).getStatusIcon().equals("[\u2713]")) {
+                throw new DukeException(Errors.ERROR_MARKED_TASK);
+            }
+            TaskList.arrTasks.get(itemIndex).markAsDone();
+            TextUi.printDone(itemIndex);
+        } catch (NumberFormatException e) {
+            TextUi.printError(Errors.ERROR_INVALID_TASK_NUMBER);
+        }
+    }
 }
