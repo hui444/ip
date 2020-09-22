f9f2b58207180bc4ee3e37688cd5b0653d7fe25d
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:51:04 +0800

    Update ToDo.java

--- src/main/java/duke/tasks/ToDo.java
+++ src/main/java/duke/tasks/ToDo.java
@@ -2,18 +2,18 @@ package duke.tasks;
 
 public class ToDo extends Task {
 
-	public ToDo(String description) {
-	    super(description);
-	}
+    public ToDo(String description) {
+        super(description);
+    }
 
-	@Override
-	public String toString() {
-	    return "[T]" + super.toString();
-	}
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 
-	@Override
-	public String fileString() {
-		int val = isDone ? 1 : 0;
-		return "T | " + val + " | " + this.description;
-	}
+    @Override
+    public String fileString() {
+        int val = isDone ? 1 : 0;
+        return "T | " + val + " | " + this.description;
+    }
 }
