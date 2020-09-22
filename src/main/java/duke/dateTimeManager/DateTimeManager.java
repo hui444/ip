7ce7d43e148d45c163e8fc2c8b268927b655ce79
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:39:31 +0800

    Update DateTimeManager.java

--- src/main/java/duke/dateTimeManager/DateTimeManager.java
+++ src/main/java/duke/dateTimeManager/DateTimeManager.java
@@ -7,30 +7,29 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeManager {
-	public static String dateTimeManager(String dateTime) throws DukeException {
-		if(dateTime.trim().split(" ")[0].contains("-")) {
-			String formattedDateTime;
-			try {
-				String[] splitData = dateTime.trim().split(" ");
-				String date = splitData[0];
+    public static String dateTimeManager(String dateTime) throws DukeException {
+        if(dateTime.trim().split(" ")[0].contains("-")) {
+            String formattedDateTime;
+            try {
+                String[] splitData = dateTime.trim().split(" ");
+                String date = splitData[0];
 
-				LocalDate formattedDate = LocalDate.parse(date);
-				formattedDateTime = formattedDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+                LocalDate formattedDate = LocalDate.parse(date);
+                formattedDateTime = formattedDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
 
-				if (splitData.length > 1) {
-					formattedDateTime += ",";
-					for(int i = 1; i < splitData.length; i++) {
-						formattedDateTime += " " + splitData[i];
-					}
-				}
+                if (splitData.length > 1) {
+                    formattedDateTime += ",";
+                    for(int i = 1; i < splitData.length; i++) {
+                        formattedDateTime += " " + splitData[i];
+                    }
+                }
 
-				return String.valueOf(formattedDateTime);
-
-			} catch (DateTimeException e) {
-				throw new DukeException(e.getMessage());
-			}
-		} else {
-			return dateTime;
-		}
-	}
+                return String.valueOf(formattedDateTime);
+            } catch (DateTimeException e) {
+                throw new DukeException(e.getMessage());
+            }
+        } else {
+            return dateTime;
+        }
+    }
 }
