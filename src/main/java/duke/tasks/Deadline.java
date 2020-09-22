993553c68f23be85ea6e1ec98ad6a87788413492
hui444 <57870728+hui444@users.noreply.github.com>
2020-09-22 20:48:37 +0800

    Update Deadline.java

--- src/main/java/duke/tasks/Deadline.java
+++ src/main/java/duke/tasks/Deadline.java
@@ -2,21 +2,21 @@ package duke.tasks;
 
 public class Deadline extends Task{
 
-	protected String deadlineDate;
+    protected String deadlineDate;
 
-	public Deadline(String description, String deadlineDate) {
-	    super(description);
-	    this.deadlineDate = deadlineDate;
-	}
+    public Deadline(String description, String deadlineDate) {
+        super(description);
+        this.deadlineDate = deadlineDate;
+    }
 
-	@Override
-	public String toString() {
-	    return "[D]" + super.toString() + " (by: " + deadlineDate + ")";
-	}
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + deadlineDate + ")";
+    }
 
-	@Override
-	public String fileString() {
-		int val = isDone ? 1 : 0;
-		return "D | " + val + " | " + this.description + " | " + this.deadlineDate;
-	}
+    @Override
+    public String fileString() {
+        int val = isDone ? 1 : 0;
+        return "D | " + val + " | " + this.description + " | " + this.deadlineDate;
+    }
 }
